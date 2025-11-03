843d622a68cfaa31232cac19f09a23dd499d4cf9
Pablo <pabl.sepulvedaa@duocuc.cl>
2025-11-03 00:32:45 -0300

    .

--- app/components/atoms/NavBarLinks.tsx
+++ app/components/atoms/NavBarLinks.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Menu } from "antd";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 
 const items = [
     { key: "1", label: <Link to="/productos">Ver Productos</Link> },
--- app/routes/AppRoutes.tsx
+++ app/routes/AppRoutes.tsx
@@ -1,23 +1,19 @@
-import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "../components/molecules/Header";
 import Home from "./home";
 import AddProductPage from "./AddProduct";
 import EditProductPage from "./EditProduct";
 
 const AppRoutes: React.FC = () => (
-
-
-<HashRouter>
-    <Header />
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/productos" element={<Home />} />
-        <Route path="/productos/agregar" element={<AddProductPage />} />
-        <Route path="/productos/editar/:id" element={<EditProductPage />} />
-    </Routes>
-</HashRouter>
-
+    <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/productos" element={<Home />} />
+            <Route path="/productos/agregar" element={<AddProductPage />} />
+            <Route path="/productos/editar/:id" element={<EditProductPage />} />
+        </Routes>
+    </BrowserRouter>
 );
 
 export default AppRoutes;
